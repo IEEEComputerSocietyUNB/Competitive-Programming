2111c20331534b6b07375436013e89a50eee6aaa
daviegito <daviegitocoelho@gmail.com>
2025-06-19 09:37:42 -0300

    feat: add alguns exercícios, alguns ainda incompletos

---
+++ daviegito/1151_fibonacci_incompleto.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int ent, i = 2,fib;
+	cin >> ent;
+	if ent(n<3) {
+		if (ent == 0) {
+		cout << "";
+		} else if (ent == 1) {
+		cout << "0";
+		} else if (ent == 2) {
+		cout << "0 1";
+		}
+	} else {
+	for(i=2;i<ent;i++){
+	
+	cout << "0 1 " << 	
+	}
+	}
+	return 0;
+}
